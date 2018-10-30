0b127e0e7da7335383b3b3fa6bd12c88a128d7a7
mazanax <mazanax@yandex.ru>
2018-10-31 01:39:23 +0300

    #5 dockerized application

--- settings.py
+++ settings.py
@@ -1,6 +1,13 @@
 from os import getenv
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 API_TOKEN = getenv('API_TOKEN')
+
+DB_USER = getenv('POSTGRES_USER')
+DB_PASS = getenv('POSTGRES_PASSWORD')
+DB_NAME = getenv('POSTGRES_DB')
+DB_HOST = getenv('POSTGRES_HOST')
+DB_PORT = getenv('POSTGRES_PORT')
